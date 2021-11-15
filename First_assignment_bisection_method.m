f6c9bc97df8601717ad5f32ca7992775881bf587
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-15 19:27:48 +0000

    Update first assignment - bissection method.

--- First_assignment_bisection_method.m
+++ First_assignment_bisection_method.m
@@ -1,16 +1,12 @@
-l = 0.01:0.01:0.05;
-ak = zeros(3,length(l));
-bk = zeros(3,length(l));
-ks = zeros(3,length(l));
-
-
+l = 0.01:0.01:0.03;
+functionCallsVaryingL = zeros(3,length(l));
 
 for i=1:1:3
-    a = -4;
-    b = 4;
     e = 0.001;
-    k = 0;
-    for j=1:1:length(l)
+    for j=1:length(l)
+        a = -4;
+        b = 4;
+        k = 0;
         while (b - a) >= l(j)
            x1 = (a + b) / 2 - e;
            x2 = (a + b) / 2 + e;
@@ -21,19 +17,88 @@ for i=1:1:3
                a = x1;
            end
            k = k + 1;
-           ak(i,k) = a;
-           bk(i,k) = b;
-           ks(i,k) = k;
+           functionCallsVaryingL(i,j) = 2*k;
+           ak(i,k,j) = a;
+           bk(i,k,j) = b;
+           ks(i,k,j) = k;
         end
     end
 end
 
-
 figure(1)
-plot(ks(1,:), ak(1,:),'o', ks(1,:), bk(1,:),'x')
+plot(l,functionCallsVaryingL(1,:))
+title('Function 1 calls for different Ls')
+
+figure(2)
+plot(l,functionCallsVaryingL(2,:))
+title('Function 2 calls for different Ls')
+
+figure(3)
+plot(l,functionCallsVaryingL(3,:))
+title('Function 3 calls for different Ls')
+
+figure(4)
+plot(ks(1,:,1), ak(1,:,1),'o', ks(1,:,1), bk(1,:,1),'x')
 hold on
-plot(ks(2,:), ak(2,:),'o', ks(2,:), bk(2,:),'x')
-plot(ks(3,:), ak(3,:),'o', ks(3,:), bk(3,:),'x')
+plot(ks(1,:,2), ak(1,:,2),'*', ks(1,:,2), bk(1,:,2),'>')
+plot(ks(1,:,3), ak(1,:,3),'d', ks(1,:,3), bk(1,:,3),'<')
+title('Function 1')
 hold off
+
+figure(5)
+plot(ks(2,:,1), ak(2,:,1),'o', ks(2,:,1), bk(2,:,1),'x')
+hold on
+plot(ks(2,:,2), ak(2,:,2),'*', ks(2,:,2), bk(2,:,2),'>')
+plot(ks(2,:,3), ak(2,:,3),'d', ks(2,:,3), bk(2,:,3),'<')
+title('Function 2')
+hold off
+
+figure(6)
+plot(ks(3,:,1), ak(3,:,1),'o', ks(3,:,1), bk(3,:,1),'x')
+hold on
+plot(ks(3,:,2), ak(3,:,2),'*', ks(3,:,2), bk(3,:,2),'>')
+plot(ks(3,:,3), ak(3,:,3),'d', ks(3,:,3), bk(3,:,3),'<')
+title('Function 3')
+hold off
+
+e = 0.0001:0.0001:0.0003;
+functionCallsVaryingE = zeros(3,length(e));
+for i=1:1:3
+    l = 0.01;
+    for j=1:length(e)
+        a = -4;
+        b = 4;
+        k = 0;
+        while (b - a) >= l
+           x1 = (a + b) / 2 - e(j);
+           x2 = (a + b) / 2 + e(j);
+
+           if funcChoose(x1,i) < funcChoose(x2,i)
+               b = x2;
+           else
+               a = x1;
+           end
+           k = k + 1;
+           functionCallsVaryingE(i,j) = 2*k;
+           ak(i,k,j) = a;
+           bk(i,k,j) = b;
+           ks(i,k,j) = k;
+        end
+    end
+end
+functionCallsVaryingE
+length(e)
+
+figure(7)
+plot(e,functionCallsVaryingE(1,:))
+title('Function 1 calls for different Es')
+
+figure(8)
+plot(e,functionCallsVaryingE(2,:))
+title('Function 2 calls for different Es')
+
+figure(9)
+plot(e,functionCallsVaryingE(3,:))
+title('Function 3 calls for different Es')
 
 
