4bcb45ccd084229302a6045cf2a7904b0e8028d0
Konstantiss <konstantinos.michopoulos@gmail.com>
2021-11-16 13:10:12 +0200

    Add funcChoose file.

---
+++ funcChoose.m
@@ -0,0 +1,11 @@
+function y = funcChoose(x,i)
+    if i == 1
+        y = (x - 3)^2 + sin(x + 3)^2 
+    elseif i == 2
+        y = (x - 1) * cos(0.5 * x) + x^2
+    elseif i == 3
+        y = (x + 2)^2 + exp(x - 2) * sin(x + 3)
+    else 
+        y = NaN
+    end
+end
