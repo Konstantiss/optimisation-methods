2f93d28ee289cf98666fcb5b5c59b08f36afb84b
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-16 17:02:38 +0000

    Add fibonacci method for first assignment.

---
+++ First_assignment_fibonacci_method.m
@@ -0,0 +1,80 @@
+clear
+clc
+
+l = 0.01:0.01:0.05;
+e = 0.001;
+functionCallsVaryingL = zeros(3,length(l));
+
+
+for i=1:1:3
+    for j=1:length(l)
+        a = -4;
+        b = 4;
+        k = 0;
+        n = 0;
+        while fibonacci(n) <= ((b - a) / l(j))
+            n = n + 1; 
+        end
+        x1 = a + (fibonacci(n - 2) / fibonacci(n)) * (b - a);
+        x2 = a + (fibonacci(n - 1) / fibonacci(n)) * (b - a);
+        for k = 1:(n - 2)
+           if funcChoose(x1,i) > funcChoose(x2,i)
+               a = x1;
+               x1 = x2;
+               x2 = a + (fibonacci(n - k - 1) / fibonacci(n - k)) * (b - a);
+           elseif funcChoose(x1,i) < funcChoose(x2,i)
+               b = x2;
+               x2 = x1;
+               x1 = a + (fibonacci(n - k - 2) / fibonacci(n - k)) * (b - a);
+           end
+           if k == n - 2
+                  x2 = x1 + e;
+                  if funcChoose(x1,i) > funcChoose(x2,i)
+                    a = x1;
+                  else
+                    b = x1;
+                  end
+           end
+           functionCallsVaryingL(i,j) = 2*k;
+           ak(i,k,j) = a;
+           bk(i,k,j) = b;
+           ks(i,k,j) = k;
+        end
+    end
+end
+
+figure(1)
+plot(l,functionCallsVaryingL(1,:))
+title('Function 1 calls for different Ls')
+
+figure(2)
+plot(l,functionCallsVaryingL(2,:))
+title('Function 2 calls for different Ls')
+
+figure(3)
+plot(l,functionCallsVaryingL(3,:))
+title('Function 3 calls for different Ls')
+
+figure(4)
+plot(ks(1,:,1), ak(1,:,1),'o', ks(1,:,1), bk(1,:,1),'x')
+hold on
+plot(ks(1,:,2), ak(1,:,2),'*', ks(1,:,2), bk(1,:,2),'>')
+plot(ks(1,:,3), ak(1,:,3),'d', ks(1,:,3), bk(1,:,3),'<')
+title('Function 1')
+hold off
+
+figure(5)
+plot(ks(2,:,1), ak(2,:,1),'o', ks(2,:,1), bk(2,:,1),'x')
+hold on
+plot(ks(2,:,2), ak(2,:,2),'*', ks(2,:,2), bk(2,:,2),'>')
+plot(ks(2,:,3), ak(2,:,3),'d', ks(2,:,3), bk(2,:,3),'<')
+title('Function 2')
+hold off
+
+figure(6)
+plot(ks(3,:,1), ak(3,:,1),'o', ks(3,:,1), bk(3,:,1),'x')
+hold on
+plot(ks(3,:,2), ak(3,:,2),'*', ks(3,:,2), bk(3,:,2),'>')
+plot(ks(3,:,3), ak(3,:,3),'d', ks(3,:,3), bk(3,:,3),'<')
+title('Function 3')
+hold off
