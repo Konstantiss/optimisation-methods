387b6554651ca2c44e00b6e6b220ec32341bb860
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-17 10:24:56 +0000

    Fix function call in Fibonacci.

--- First-assignment/First_assignment_fibonacci_method.m
+++ First-assignment/First_assignment_fibonacci_method.m
@@ -10,7 +10,6 @@ for i=1:1:3
     for j=1:length(l)
         a = -4;
         b = 4;
-        k = 0;
         n = 0;
         while fibonacci(n) <= ((b - a) / l(j))
             n = n + 1; 
@@ -18,18 +17,22 @@ for i=1:1:3
         x1 = a + (fibonacci(n - 2) / fibonacci(n)) * (b - a);
         x2 = a + (fibonacci(n - 1) / fibonacci(n)) * (b - a);
         for k = 1:(n - 2)
-           if funcChoose(x1,i) > funcChoose(x2,i)
+           fx1 = funcChoose(x1,i);
+           fx2 = funcChoose(x2,i);
+           if fx1 > fx2
                a = x1;
                x1 = x2;
                x2 = a + (fibonacci(n - k - 1) / fibonacci(n - k)) * (b - a);
-           elseif funcChoose(x1,i) < funcChoose(x2,i)
+           elseif fx1 < fx2
                b = x2;
                x2 = x1;
                x1 = a + (fibonacci(n - k - 2) / fibonacci(n - k)) * (b - a);
            end
            if k == n - 2
                   x2 = x1 + e;
-                  if funcChoose(x1,i) > funcChoose(x2,i)
+                  fx1 = funcChoose(x1,i);
+                  fx2 = funcChoose(x2,i);
+                  if fx1 > fx2
                     a = x1;
                   else
                     b = x1;
@@ -42,7 +45,7 @@ for i=1:1:3
         end
     end
 end
-
+functionCallsVaryingL
 figure(1)
 plot(l,functionCallsVaryingL(1,:))
 title('Function 1 calls for different Ls')
@@ -60,7 +63,7 @@ plot(ks(1,:,1), ak(1,:,1),'o', ks(1,:,1), bk(1,:,1),'x')
 hold on
 plot(ks(1,:,2), ak(1,:,2),'*', ks(1,:,2), bk(1,:,2),'>')
 plot(ks(1,:,3), ak(1,:,3),'d', ks(1,:,3), bk(1,:,3),'<')
-title('Function 1')
+title('Function 1 boundaries')
 hold off
 
 figure(5)
@@ -68,7 +71,7 @@ plot(ks(2,:,1), ak(2,:,1),'o', ks(2,:,1), bk(2,:,1),'x')
 hold on
 plot(ks(2,:,2), ak(2,:,2),'*', ks(2,:,2), bk(2,:,2),'>')
 plot(ks(2,:,3), ak(2,:,3),'d', ks(2,:,3), bk(2,:,3),'<')
-title('Function 2')
+title('Function 2 boundaries')
 hold off
 
 figure(6)
@@ -76,5 +79,5 @@ plot(ks(3,:,1), ak(3,:,1),'o', ks(3,:,1), bk(3,:,1),'x')
 hold on
 plot(ks(3,:,2), ak(3,:,2),'*', ks(3,:,2), bk(3,:,2),'>')
 plot(ks(3,:,3), ak(3,:,3),'d', ks(3,:,3), bk(3,:,3),'<')
-title('Function 3')
+title('Function 3 boundaries')
 hold off
