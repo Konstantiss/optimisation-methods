9e7700b6d5b840b8c95840899ea9a1864716670f
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-17 10:22:59 +0000

    Fix bisection method.

--- First-assignment/First_assignment_bisection_method.m
+++ First-assignment/First_assignment_bisection_method.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-l = 0.01:0.01:0.03;
+l = 0.01:0.01:0.09;
 functionCallsVaryingL = zeros(3,length(l));
 
 for i=1:1:3
@@ -13,10 +13,11 @@ for i=1:1:3
         while (b - a) >= l(j)
            x1 = (a + b) / 2 - e;
            x2 = (a + b) / 2 + e;
-
-           if funcChoose(x1,i) < funcChoose(x2,i)
+           fx1 = funcChoose(x1,i);
+           fx2 = funcChoose(x2,i);
+           if fx1 < fx2
                b = x2;
-           else
+           elseif fx1 > fx2
                a = x1;
            end
            k = k + 1;
@@ -45,7 +46,7 @@ plot(ks(1,:,1), ak(1,:,1),'o', ks(1,:,1), bk(1,:,1),'x')
 hold on
 plot(ks(1,:,2), ak(1,:,2),'*', ks(1,:,2), bk(1,:,2),'>')
 plot(ks(1,:,3), ak(1,:,3),'d', ks(1,:,3), bk(1,:,3),'<')
-title('Function 1')
+title('Function 1 boundaries')
 hold off
 
 figure(5)
@@ -53,7 +54,7 @@ plot(ks(2,:,1), ak(2,:,1),'o', ks(2,:,1), bk(2,:,1),'x')
 hold on
 plot(ks(2,:,2), ak(2,:,2),'*', ks(2,:,2), bk(2,:,2),'>')
 plot(ks(2,:,3), ak(2,:,3),'d', ks(2,:,3), bk(2,:,3),'<')
-title('Function 2')
+title('Function 2 boundaries')
 hold off
 
 figure(6)
@@ -61,10 +62,10 @@ plot(ks(3,:,1), ak(3,:,1),'o', ks(3,:,1), bk(3,:,1),'x')
 hold on
 plot(ks(3,:,2), ak(3,:,2),'*', ks(3,:,2), bk(3,:,2),'>')
 plot(ks(3,:,3), ak(3,:,3),'d', ks(3,:,3), bk(3,:,3),'<')
-title('Function 3')
+title('Function 3 boundaries')
 hold off
 
-e = 0.0001:0.0001:0.0003;
+e = 0.0001:0.0001:0.0049;
 functionCallsVaryingE = zeros(3,length(e));
 for i=1:1:3
     l = 0.01;
@@ -75,10 +76,11 @@ for i=1:1:3
         while (b - a) >= l
            x1 = (a + b) / 2 - e(j);
            x2 = (a + b) / 2 + e(j);
-
-           if funcChoose(x1,i) < funcChoose(x2,i)
+           fx1 = funcChoose(x1,i);
+           fx2 = funcChoose(x2,i);
+           if fx1 < fx2
                b = x2;
-           else
+           elseif fx1 > fx2
                a = x1;
            end
            k = k + 1;
@@ -90,8 +92,6 @@ for i=1:1:3
     end
 end
 functionCallsVaryingE
-length(e)
-
 figure(7)
 plot(e,functionCallsVaryingE(1,:))
 title('Function 1 calls for different Es')
@@ -103,5 +103,6 @@ title('Function 2 calls for different Es')
 figure(9)
 plot(e,functionCallsVaryingE(3,:))
 title('Function 3 calls for different Es')
+
 
 
