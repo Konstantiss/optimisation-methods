801910f837be8009dd41c6b54128c3ca9e1df20b
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-17 10:25:31 +0000

    Fix function call in golden section method.

--- First-assignment/First_assignment_golden_section_method.m
+++ First-assignment/First_assignment_golden_section_method.m
@@ -15,12 +15,13 @@ for i=1:1:3
         while (b - a) >= l(j)
            x1 = a + (1 - gamma) * (b - a);
            x2 = a + gamma * (b - a);
-
-           if funcChoose(x1,i) > funcChoose(x2,i)
+           fx1 = funcChoose(x1,i);
+           fx2 = funcChoose(x2,i);
+           if fx1 > fx2
                a = x1;
                x2 = a + gamma * (b - a);
                x1 = x2;
-           elseif funcChoose(x1,i) < funcChoose(x2,i)
+           elseif fx1 < fx2
                b = x2;
                x2 = x1;
                x1 = a + (1 - gamma) * (b - a);
@@ -51,7 +52,7 @@ plot(ks(1,:,1), ak(1,:,1),'o', ks(1,:,1), bk(1,:,1),'x')
 hold on
 plot(ks(1,:,2), ak(1,:,2),'*', ks(1,:,2), bk(1,:,2),'>')
 plot(ks(1,:,3), ak(1,:,3),'d', ks(1,:,3), bk(1,:,3),'<')
-title('Function 1')
+title('Function 1 boundaries')
 hold off
 
 figure(5)
@@ -59,7 +60,7 @@ plot(ks(2,:,1), ak(2,:,1),'o', ks(2,:,1), bk(2,:,1),'x')
 hold on
 plot(ks(2,:,2), ak(2,:,2),'*', ks(2,:,2), bk(2,:,2),'>')
 plot(ks(2,:,3), ak(2,:,3),'d', ks(2,:,3), bk(2,:,3),'<')
-title('Function 2')
+title('Function 2 boundaries')
 hold off
 
 figure(6)
@@ -67,6 +68,5 @@ plot(ks(3,:,1), ak(3,:,1),'o', ks(3,:,1), bk(3,:,1),'x')
 hold on
 plot(ks(3,:,2), ak(3,:,2),'*', ks(3,:,2), bk(3,:,2),'>')
 plot(ks(3,:,3), ak(3,:,3),'d', ks(3,:,3), bk(3,:,3),'<')
-title('Function 3')
+title('Function 3 boundaries')
 hold off
-
