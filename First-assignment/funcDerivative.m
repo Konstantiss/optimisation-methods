46206be453062cea37372f9c965aeee68ce18e1d
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-17 09:39:29 +0000

    Fix funcDerivative.

--- First-assignment/funcDerivative.m
+++ First-assignment/funcDerivative.m
@@ -1,10 +1,17 @@
-function y = funcDerivative(x,i)
+function y = funcChoose(x,i)
+    syms z;
     if i==1
-        y = 2*x + 2*cos(x + 3)*sin(x + 3) - 6;
+        f = (z-3)^2 + sin(z+3)^2;
+        derivative = diff(f);
+        y = vpa(subs(derivative,z,x));
     elseif i==2
-        y = 2*x + cos(x/2) - (sin(x/2)*(x - 1))/2;
+        f = (z-1) * cos(0.5*z) + z^2;
+        derivative = diff(f);
+        y = vpa(subs(derivative,z,x));
     elseif i==3
-        y = 2*x + cos(x + 3)*exp(x - 2) + sin(x + 3)*exp(x - 2) + 4;
+        f = (z+2)^2 + exp(z-2) * sin(z+3);
+        derivative = diff(f);
+        y = vpa(subs(derivative,z,x));
     else
         y = NaN
     end
